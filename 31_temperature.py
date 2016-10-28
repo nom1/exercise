d507f1a91fd4be0b037db2b0416f4f4e8a95ca67
nom1 <numoru1.314@lgflmail.net>
2016-10-28 16:05:42 +0100

    Update 31_temperature.py

--- 31_temperature.py
+++ 31_temperature.py
@@ -12,15 +12,6 @@ def temperature():
         C = int(raw_input("please enter the temperature in celsius: "))
         sum2 = (C*9/5)+32
         print(sum2)
-    # ce = (F-32)*5/9
-    # fa = (C*9/5)+32
-    # choice = input("your choice: ")
-    # if choice == C:
-    #     F = int(input("please enter the temperature in fahrenheit: "))
-    #     print("The temperature in celsius is"+ ce)
-    # else:
-    #     C = int(input("please enter the temperature in celsius: "))
-    #     print("The temperature in celsius is" + fa)
-
+  
 
 temperature()
